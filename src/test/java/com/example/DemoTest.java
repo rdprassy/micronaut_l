61dd852ae9a2d03c95c0354e2149ec0887089b81
rdprassy <rdprassy@gmail.com>
2021-09-11 19:41:55 +0530

    wrote test cases

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-import jakarta.inject.Inject;
+import javax.inject.Inject;
+
+import io.micronaut.http.client.RxHttpClient;
 
 @MicronautTest
 class DemoTest {
@@ -13,9 +16,18 @@ class DemoTest {
     @Inject
     EmbeddedApplication<?> application;
 
+    @Inject
+    @Client("/") RxHttpClient client;
+
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testHelloResponse() {
+        final String result = client.toBlocking().retrieve("/hello");
+        Assertions.assertEquals("Hello World", result);
     }
 
 
