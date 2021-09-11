4ada084a4460ea88f13d6dcb2bb494eb040b1772
rdprassy <rdprassy@gmail.com>
2021-09-11 23:03:21 +0530

    Added Hello world service logging and application logging

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,15 +2,21 @@ package com.example;
 
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
 
         final ApplicationContext context = Micronaut.run(Application.class);
         final HelloWorldService service = context.getBean(HelloWorldService.class);
         System.out.println(service.sayHi());
-        context.close();
+        LOG.info(service.sayHi());
+//        context.close();
 
 
 //        Micronaut.run(Application.class, args);
