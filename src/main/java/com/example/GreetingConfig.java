4d2ff334db785f40d1d5361395671f69487f5aa3
rdprassy <rdprassy@gmail.com>
2021-09-12 09:10:36 +0530

    added lombok to the project

--- src/main/java/com/example/GreetingConfig.java
+++ src/main/java/com/example/GreetingConfig.java
@@ -3,12 +3,16 @@ package com.example;
 
 import io.micronaut.context.annotation.ConfigurationInject;
 import io.micronaut.context.annotation.ConfigurationProperties;
+import lombok.Getter;
 
 //See how you are mapping the application.yml config properties to be picked from here
 @ConfigurationProperties("hello.config.greeting")
 public class GreetingConfig {
 
+    @Getter
     private final String de;
+
+    @Getter
     private final String en;
 
 
@@ -18,11 +22,11 @@ public class GreetingConfig {
         this.en = en;
     }
 
-    public String getDe() {
-        return de;
-    }
-
-    public String getEn() {
-        return en;
-    }
+//    public String getDe() {
+//        return de;
+//    }
+//
+//    public String getEn() {
+//        return en;
+//    }
 }
